2f925b2d99ef4e83f8be6f2e2e2d106b340edd4f
Kristina123455 <kristinaneskovic9@gmail.com>
2021-12-22 10:55:24 +0100

    log out link modified

--- settings.php
+++ settings.php
@@ -26,7 +26,7 @@
                     <div class="dropdown-menu">
                         <a href="profile.php"><img src="images/saved.png" alt="saved">Saved movies</a>
                         <a href="settings.php">My profile</a>
-                        <a href="index.php">Log out <img src="images/logout.png" alt="out"></a>
+                        <a href="php/logout.php">Log out <img src="images/logout.png" alt="out"></a>
                     </div>
                     </div>
                 </li>
@@ -38,7 +38,7 @@
                 <a href="aboutUs.php#contactus">Contact</a>
                 <a href="profile.php"><img src="images/saved.png" alt="saved">Saved movies</a>
                 <a href="settings.php">My profile</a>
-                <a href="index.php">Log out <img src="images/logout.png" alt="out"></a>
+                <a href="php/logout.php">Log out <img src="images/logout.png" alt="out"></a>
                 </div>
                 </div>
                 </li>
