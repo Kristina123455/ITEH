87425d9055cfa59308bce16b0fbbfed678d0d3a3
Kristina123455 <kristinaneskovic9@gmail.com>
2021-12-22 10:56:18 +0100

    register user

--- php/registerUser.php
+++ php/registerUser.php
@@ -1,26 +1,28 @@
 <?php
+session_start();
 include("databaseBroker.php");
-
-extract($_POST);
-$query = "SELECT * FROM user where email='$email'";
-$sql=mysqli_query($conn,$query);
-if(mysqli_num_rows($sql)>0)
+if(isset($_POST['register']))
 {
-    echo "Email Already Taken, try again."; 
-	exit;
-}
-if(isset($_POST['register'])){
+    extract($_POST);
+    $query = "SELECT * FROM user where email='$email'";
+    $sql=mysqli_query($conn,$query);
+    if(mysqli_num_rows($sql)>0)
+    {
+    echo "Sorry, " . $_POST["firstName"] . " Email Already Taken," ?> <a href="../register.php">try again</a> <?php "."; 
+	exit();
+    }
+    else {
         $query="INSERT INTO user VALUES (null,'$firstName', '$lastName', '$email', '$password')";
         $sql=mysqli_query($conn,$query);
-        $_SESSION["user_email"] = $row['email'];
-        $_SESSION["user_pass"]=$row['password'];
-        $_SESSION["user_firstname"] = $row['first_name'];
-        $_SESSION["user_lastname"] = $row['last_name'];
-        if(!$sql){ die("Could Not Perform the Query");};
+        $_SESSION["user_email"] = $_POST['email'];
+        $_SESSION["user_pass"]=$_POST['password'];
+        $_SESSION["user_firstname"] = $_POST['firstName'];
+        $_SESSION["user_lastname"] = $_POST['lastName'];
+        
+    }
+    if(isset($_SESSION["user_email"])){
         header ("Location: ../movies.php");
-}
-else {
-    echo "Error.Please try again";
-}
+        }
 
+}
 ?>
