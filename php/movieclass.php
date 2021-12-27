2ad2801962c963a8b95e56461b6f0a72bb44c5c1
Kristina123455 <kristinaneskovic9@gmail.com>
2021-12-27 15:44:15 +0100

    INNER JOIN

--- php/movieclass.php
+++ php/movieclass.php
@@ -19,7 +19,15 @@ class Movie{
     //displaying all saved movies
     public static function getAll(mysqli $conn){
 
-        $query = "SELECT * FROM saved_movies";
+        $query = "SELECT * FROM `saved_movies` INNER JOIN `category` ON saved_movies.category=category.category_id ";
+        return $conn->query($query);
+
+    }
+
+    //displaying all best movies
+    public static function getAllBestMovies(mysqli $conn){
+
+        $query = "SELECT * FROM `best_movies` INNER JOIN `category` ON best_movies.movie_category=category.category_id ";
         return $conn->query($query);
 
     }
@@ -38,11 +46,40 @@ class Movie{
     }
 
 
-    public function deleteById(mysqli $conn){
-        $query = "DELETE FROM saved_movies WHERE movie_id = '$this->id'";
+    public static function deleteById($id,mysqli $conn){
+        $query = "DELETE FROM saved_movies WHERE movie_id = '$id'";
         return $conn->query($query);
     }
 
+    public static function insertById($id, mysqli $conn){
+
+        $query = "SELECT * FROM best_movies WHERE movie_id='$id'";
+        $selectedMovie = mysqli_query($conn, $query);
+        $row  = mysqli_fetch_array($selectedMovie);
+        if(is_array($row)){
+            $id=$row['movie_id'];
+            $title = $row['movie_title'];
+            $category = $row['movie_category'];
+            $actors= $row['actors'];
+            $query = "INSERT INTO saved_movies (movie_id, movie_title, category, actors) VALUES ('$id','$title','$category','$actors')";
+            return $conn->query($query);
+        }
+
+    }
+
+    public static function getDescription($id, mysqli $conn){
+
+        $query = "SELECT movie_desc FROM best_movies WHERE movie_id='$id'";
+        $selectedMovie = mysqli_query($conn, $query);
+        $row  = mysqli_fetch_array($selectedMovie);
+        if(is_array($row)){
+           
+            $desc= $row['movie_desc'];
+            
+            return $desc;
+        }
+
+    }
 
 }
 
