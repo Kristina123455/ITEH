ea6288e440ca69055a2406a55c3d01bc78334c6d
Kristina123455 <kristinaneskovic9@gmail.com>
2021-12-27 15:44:56 +0100

    php for AJAX (delete saved movie)

---
+++ php/deleteSavedMovie.php
@@ -0,0 +1,16 @@
+<?php 
+
+include "databaseBroker.php";
+include "movieclass.php";
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $status= Movie::deleteById($id,$conn);
+    if ($status){
+        echo "Success";
+    }else{
+        echo "Failed";
+    }
+}
+
+?>
